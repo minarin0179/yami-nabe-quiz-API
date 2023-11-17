76c5b765639aacef01d3fa05b05d0c64fe437100
minarin0179 <takissyd@gmail.com>
2023-11-18 00:39:52 +0900

    完成

--- src/app.ts
+++ src/app.ts
@@ -3,62 +3,89 @@ import bodyParser from 'body-parser';
 import { readJSONLFile } from './readJSONL.js';
 import { generate } from './generate.js';
 import fs from 'fs';
+import path from 'path';
 import * as https from 'https';
 import cors from 'cors';
+import { Questions, getRandomQuestions, sequelize } from './sql.js';
 
 const filePath = 'train_questions.json';
+const reactPath = "/home/ubuntu/yami-nabe-quiz-react";
 
 const app = express();
-const port = 3000;
+const port = 443;
 const questions = await readJSONLFile(filePath);
 
-const privateKey = fs.readFileSync('privatekey.pem', 'utf8');
-const certificate = fs.readFileSync('cert.pem', 'utf8');
+const privateKey = fs.readFileSync('privkey.pem', 'utf8');
+const certificate = fs.readFileSync('fullchain.pem', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
 const server = https.createServer(credentials, app);
 
 // CORSを許可する
-app.use(cors({
-    origin:'https://minarin0179.github.io'
-}));
+app.use(cors());
 
 // JSONのリクエストを解析するためのミドルウェア
 app.use(bodyParser.json());
 
-// POSTリクエストを受け取るエンドポイント
+// Reactアプリケーションの静的ファイルを提供
+app.use(express.static(path.join(reactPath, 'build')));
+
 app.get('/api', async (req, res) => {
-    // リクエストデータを取得
+    try {
+        // リクエストデータを取得
+        let limit = 4;
+        if (req.query.size && typeof req.query.size === 'string') limit = parseInt(req.query.size);
 
-    let size = 4;
-    if (req.query.size && typeof req.query.size === 'string') size = parseInt(req.query.size);
-    console.log(req.query);
-    console.log(size);
+        if (limit < 1 || limit > 6) throw new Error('Invalid size.');
 
-    const randomNumbers = [...Array(size)].map(() => Math.floor(Math.random() * questions.length));
-    const ingredients = randomNumbers.map((randomNumber) => {
-        const selected = questions[randomNumber];
-        if (!('question' in selected) || !('answer_entity' in selected)) throw new Error('Invalid data');
-        const { question, answer_entity } = selected;
-        if (typeof question !== 'string' || typeof answer_entity !== 'string') throw new Error('Invalid type');
-        return {
-            question: question,
-            answer: answer_entity,
+        const ingredients = (await getRandomQuestions(limit)).filter(i => 'question' in i);
+        const generator = await generate(ingredients.map(({ question }) => question));
+
+        if (!generator || !generator.result) {
+            // generateに失敗した場合のエラーハンドリング
+            throw new Error('Failed to generate question.');
         }
 
-    });
-    const generator = await generate(ingredients.map(({ question }) => question))
-    //TODO generateに失敗したらエラーコードを返す
+        // レスポンスデータを作成
+        const responseData = {
+            question: generator.result,
+            ingredients: ingredients,
+        };
 
-    // レスポンスデータを作成
-    const responseData = {
-        question: generator.result,
-        ingredients: ingredients,
-    };
-
-    // レスポンスをJSON形式で返す
-    res.json(responseData);
+        // レスポンスをJSON形式で返す
+        res.json(responseData);
+    } catch (error) {
+        // エラーハンドリング
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
+
+
+// 材料を投入
+app.post('/api', async (req, res) => {
+    try {
+        const { question, answer } = req.body;
+
+        // データベースにデータを挿入
+        const newQuestion = await Questions.create({ question, answer });
+
+        res.json({
+            status: 'success',
+            message: 'データを挿入しました',
+            data: newQuestion
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('サーバーエラー');
+    }
+})
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(reactPath, 'build', 'index.html'));
+});
+
 
 // サーバーを起動
 server.listen(port, () => {
